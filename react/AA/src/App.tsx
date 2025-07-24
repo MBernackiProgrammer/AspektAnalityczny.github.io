7c863283ec33fd246c0db1ee6b814e8496d978b0
Miłosz <bugiel.milosz@gmail.com>
2025-07-24 11:36:33 +0200

    build added

--- react/AA/src/App.tsx
+++ react/AA/src/App.tsx
@@ -1,5 +1,5 @@
 
-import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import './App.css'
 import Index from './components/page';
